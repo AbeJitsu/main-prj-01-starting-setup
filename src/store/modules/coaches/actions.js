d26cad65357ed305b02891d13fd13a1ce4f8c3cc
Abe Reyes <abe.raise@gmail.com>
2024-01-13 00:27:21 -0500

    Refactor registerCoach action to use async/await and fetch API

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-    registerCoach(context, data) {
-        const coachData = {
-            id: context.rootGetters.userId,
-            firstName: data.first,
-            lastName: data.last,
-            description: data.desc,
-            hourlyRate: data.rate,
-            areas: data.areas
-};
-context.commit('registerCoach', coachData)
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      // id: context.rootGetters.userId,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+
+    const response = await fetch(
+      `https://techtutorteam-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    if (!response.ok) {
+      // error ...
     }
-}
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+};

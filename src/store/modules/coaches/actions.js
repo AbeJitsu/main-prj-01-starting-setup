733ed5af59791809d58f9e9137e2b1690485323c
Abe Reyes <abe.raise@gmail.com>
2024-01-13 18:27:55 -0500

    Fix button click event and add error handling

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -21,37 +21,39 @@ export default {
       // error ...
     }
 
-
     context.commit('registerCoach', {
       ...coachData,
       id: userId,
     });
   },
 
-
-    
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
-      `https://techtutorteam-default-rtdb.firebaseio.com/coaches/.json`);
-      const responseData = await response.json();
+      `https://techtutorteam-default-rtdb.firebaseio.com/coaches/.json`
+    );
+    const responseData = await response.json();
 
-      if (!response.ok) {
-        const error = new Error(responseData.message || 'Failed to fetch!')
-        throw error;
-      }    
-      const coaches = [];
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+    const coaches = [];
 
-      for (const key in responseData) {
-        const coach = {
-            id: key, 
-            firstName: responseData[key].firstName,
-            lastName: responseData[key].lastName,
-            description: responseData[key].description,
-            hourlyRate: responseData[key].hourlyRate,
-            areas: responseData[key].areas
-        };
-        coaches.push(coach);
-      }
-      context.commit('setCoaches', coaches);
-  }
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
+  },
 };
